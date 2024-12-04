661a28f7c2ee69226a9aced546ae59595525a360
石天然 Ted <teddyshih5678@gmail.com>
2024-12-04 16:33:52 +0800

    父傳子

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-child',
@@ -8,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrl: './child.component.css'
 })
 export class ChildComponent {
+@Input() parentname:string = '';
 
 }
