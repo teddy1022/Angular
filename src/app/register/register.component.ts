16267e19ec832674123983cda26dafeeaea183d1
石天然 Ted <teddyshih5678@gmail.com>
2024-12-04 17:39:31 +0800

    表單驗證

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
-
+  registerForm = new FormGroup({
+    senderName:new FormControl('', Validators.required),
+    senderEmail:new FormControl('', [Validators.required, Validators.email]),
+    senderPassword:new FormControl('', [Validators.required, Validators.minLength(6)]),
+  })
 }
